a2eea41ee1b3c01941ad1a20eb9bbc56f938f7fb
강태훈 <HI@gangtaehun-ui-MacBook-Pro.local>
2018-03-14 20:03:16 +0900

    commit test

--- RxSwift_1/ViewController.swift
+++ RxSwift_1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        debugPrint("commit test log")
     }
 
     override func didReceiveMemoryWarning() {
